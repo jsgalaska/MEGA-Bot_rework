a3a02ba9c443d77e582e196b24b5ea9b66f27d19
DAMedina <vipergtsr009@hotmail.com>
2017-05-18 21:11:18 -0400

    bot: Updated parsing

--- bot.py
+++ bot.py
@@ -3,11 +3,13 @@ import cfg, socket, re, time, sys
 from irc_control import Controller
 from msg_parser import GET_THIS, PARSE_THIS
 
+
 HOST = cfg.HOST
 PORT = cfg.PORT
 NICK = cfg.NICK
 PASS = cfg.PASS
 CHAN = cfg.CHAN
+
 '''DB_HOST = cfg.DB_HOST
 DB_NAME = cfg.DB_NAME
 DB_USER = cfg.DB_USER
@@ -17,6 +19,18 @@ DB_VIEWERS_TABLE = cfg.DB_VIEWERS_TABLE'''
 ENGAGE = False
 sec = cfg.sec # ◄ Set desired seconds to wait for script termination (0 is 1 sec)
 MAXSENDINTERVAL = 20.0/30
+
+data = ""
+s = socket.socket()
+irc = Controller(HOST, PORT, s)
+irc.send_pass(cfg.PASS)
+irc.send_nick(cfg.NICK)
+irc.capreq_membership()
+irc.capreq_commands()
+#capreq_tags() -Works, but breaks adminList commands; possibly other things
+irc.join_channel(cfg.CHAN)
+msg, sender = " ", " "
+
     
 #------------------------------------------------▼ Terminate Script Timer
 
@@ -32,22 +46,6 @@ def countdown(sec):
         sys.exit()
 
 #------------------------------------------------
-
-data = ""
-s = socket.socket()
-irc = Controller(HOST, PORT, s)
-irc.send_pass(cfg.PASS)
-irc.send_nick(cfg.NICK)
-irc.capreq_membership()
-irc.capreq_commands()
-#capreq_tags() -Works, but breaks adminList commands; possibly other things
-irc.join_channel(cfg.CHAN)
-
-#testing------------------------------
-msg, sender = GET_THIS, PARSE_THIS
-getData = GET_THIS(msg, sender)
-parseData = PARSE_THIS(msg, sender)
-#-------------------------------------
 
 print('Initializing')
 
@@ -66,24 +64,22 @@ while True:
             if len(line) >= 1:
                 if line[0] == 'PING':
                     irc.send_pong(line[1])
-
-                #testing------------------------------
+                    
                 if line[1] == 'PRIVMSG':
-                    sender = getData.get_sender(line[0])
-                    message = getData.get_message(line)
-                    parseData.parse_message(sender, message)
+                    sender = GET_THIS(msg, sender).get_sender(line[0])
+                    message = GET_THIS(msg, sender).get_message(line)
+                    PARSE_THIS(msg, sender).parse_message(sender, message)
                     print('CHAT> '+sender +": " + message)
 
                 if line[1] == 'JOIN':
-                    sender = getData.get_sender(line[0])
+                    sender = GET_THIS(msg, sender).get_sender(line[0])
                     #save_to_db(sender) ---------save & uncomment
                     print('▌VIEWER UPDATE: ' +'|' +sender +'|' +' has joined the chat!')
                     #send_message(CHAN, 'Welcome '+sender+'! Ya Scrub') -Turned off. May scare/trigger people
 
                 if line[1] == 'PART':
-                    sender = getData.get_sender(line[0])
+                    sender = GET_THIS(msg, sender).get_sender(line[0])
                     print('▌VIEWER UPDATE: '+sender +' has left the chat! :(')
-                #-------------------------------------
                 
             while ENGAGE == False:
                 print('I have arrived in ' + CHAN + "'s channel!")
